8307eed128b770ff8a4b9324d8d7932c643a12cc
Edward979797 <edwardceapcin@gmail.com>
2022-04-19 01:13:13 -0400

    Did some extra work on highlights section, including timeline component

--- src/components/PageText.js
+++ src/components/PageText.js
@@ -6,13 +6,32 @@ export default function PageText() {
         <div className="pagetext">
             <div className="pagetext--highlights">
                 <h3>Highlights</h3>
-                    <ul>
-                        <li>Aljihsssdlqhieiblbeleibleib ielbciogii elh ciehcoigegio ih iehc iegcgc ice cioegcoeg</li>
-                        <li>Aljihsssdlqhieiblbeleibleib ielbciogii elh ciehcoigegio ih iehc iegcgc ice cioegcoeg</li>
-                        <li>Aljihsssdlqhieiblbeleibleib ielbciogii elh ciehcoigegio ih iehc iegcgc ice cioegcoeg</li>
-                        <li>Aljihsssdlqhieiblbeleibleib ielbciogii elh ciehcoigegio ih iehc iegcgc ice cioegcoeg</li>
-                        <li>Aljihsssdlqhieiblbeleibleib ielbciogii elh ciehcoigegio ih iehc iegcgc ice cioegcoeg</li>
-                    </ul>
+                <div className="highlights-timeline">
+                    <div className="highlights-container container-left">
+                        <div className="highlights-content">
+                            <h2>2016</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                    <div className="highlights-container container-right">
+                        <div className="highlights-content">
+                            <h2>2017</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                    <div className="highlights-container container-left">
+                        <div className="highlights-content">
+                            <h2>2018</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                    <div className="highlights-container container-right">
+                        <div className="highlights-content">
+                            <h2>2019</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="pagetext--experience">
                 <h3>Experience</h3>
